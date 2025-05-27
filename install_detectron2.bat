1f33b95a68bede63460ba550d3b9f53916b49c3d
abcdefu366 <116504055+abcdefu366@users.noreply.github.com>
2025-05-27 13:45:06 +0300

    Add files via upload

---
+++ install_detectron2.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Установка detectron2 для Windows
+pip install git+https://github.com/facebookresearch/detectron2.git
---
+++ install_detectron2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Установка detectron2 для Unix-систем
+pip install 'git+https://github.com/facebookresearch/detectron2.git'
